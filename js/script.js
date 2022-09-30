2613e32083d257b171f53cd5be6cac9cdafc9055
Boris Grozdic <grozdic.borisz@gmail.com>
2022-09-30 15:53:21 +0200

    need to change colors

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// ADDING OBJECTS 
+
+let result = JSON.parse(tasks);
+
+function update(){
+for (let res of result){
+    document.getElementById("taskContent").innerHTML += `
+    <div class="col p-2">
+        <div class="card" style="width: 18rem;">
+            <img src="${res.img}" class="card-img-top">
+            <div class="card-body">
+                 <h5 class="card-title">${res.taskName}</h5>
+                <p class="card-text">${res.description}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                 <li class="list-group-item"><i class="fa fa-exclamation-triangle" style="font-size:20px"></i><p class="importanceAmount">Priority level:${res.pLevel} </p> <button class="importance-btn">Importance</button></li>
+                <li class="list-group-item">Deadline:${res.deadline}</li>
+            </ul>
+            <div class="card-body">
+                <button type="button" class="btn btn-success">Done</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+    `
+}}
+update();
+addEvent();
+
+// MAX TILL 10
+
+function numbers(index){
+    if(result[index].pLevel !=10){
+    result[index].pLevel++;
+    document.getElementsByClassName("importanceAmount")[index].innerHTML = "Priority level:" + result[index].pLevel;
+}}
+
+// ADDING NUMBERS
+
+function addEvent(){
+    let importanceBtn = document.getElementsByClassName("importance-btn");
+    for(let i = 0; i<importanceBtn.length; i++){
+    importanceBtn[i].addEventListener("click", function(){
+        numbers(i);
+    })
+}}
+
+// SORT FUNCTION
+
+document.getElementById("sortBtn").onclick = sortByValue
+function sortByValue(){
+    result.sort((a, b) => a.pLevel - b.pLevel);
+    document.getElementById("taskContent").innerHTML = "";
+    update();
+    addEvent();
+}
+
+// CHANGING COLORS
+
+
+
+
+
+
+function changingColor(){
+  let numbersColors = res.pLevel
+    if (numbersColors< 5 ){
+        
+        document.getElementsByClassName("importanceAmount").innerHTML= `<span style= "color:green">${res.pLevel}</span>`
+    } else if(numbersColors>5) {
+        document.getElementsByClassName("importanceAmount").innerHTML= `<span style= "color:red">${res.pLevel}}</span> `
+    } 
+}
+
+document.getElementsByClassName("importanceAmount").addEventListener("click", changingColor);
+
